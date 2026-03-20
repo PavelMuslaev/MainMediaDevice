ab071feb3e2e276c497abd9de3f66b34127886f5
Павел Муслаев <muslaevpavel@gmail.com>
2026-03-20 22:48:00 +0300

    Create classmethod Device.from_dict()

--- src/device/device.py
+++ src/device/device.py
@@ -179,3 +179,24 @@ class Device(ABC):
         """Возвращает строковый отчёт об объекте."""
         return (f'Device(brand={self.brand}, model={self.model}, category={self.category}, '
                 f'year={self.year}, image={self.image}, specs={self.specs}, review={self.review})')
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Device | None:
+        base_keys = ['brand', 'model', 'category']
+        all_keys = ['brand', 'model', 'category', 'year', 'image', 'specs', 'review']
+        data = copy.deepcopy(data)
+        new_device = {}
+
+        for key in base_keys:
+            if key not in data:
+                print(f'ПРОПУЩЕН БАЗОВЫЙ КЛЮЧ: {key}! 🚨')  # TODO: ЗАМЕНИТЬ НА ИСКЛЮЧЕНИЕ
+                return None
+
+        for key in all_keys:
+            if key == 'review' and key in data:
+                if isinstance(data[key], dict):
+                    new_device[key] = Review(**data[key])
+            else:
+                new_device[key] = data.get(key, None)
+
+        return cls(**new_device)
